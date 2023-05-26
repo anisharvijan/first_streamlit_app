12bf2b79bcd0e582e2a111221cf682ebcff40ee3
Anisha Raja Vijan <121279790+anisharvijan@users.noreply.github.com>
2023-05-26 11:51:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.title('Breakfast Menu')
-streamlit.line('Omega3 and Blueberry oatmeal')
+streamlit.title('Omega3 and Blueberry oatmeal')
